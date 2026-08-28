2002e99f9bb7254a3213b299f74306814cec2e97
Joselito <eu@joseli.to>
2026-08-28 14:25:18 -0300

    fix: give the domain errors a real message
    BaseError called super() with no arguments, so error.message was empty
    and the errors[] entries in the cron response carried nothing useful.

--- src/errors.ts
+++ src/errors.ts
@@ -3,17 +3,24 @@ import { Post } from "./types";
 class BaseError extends Error {
   public post: Post;
 
-  constructor(post: Post) {
-    super();
-    this.name = "BaseError";
+  constructor(post: Post, message: string) {
+    super(message);
     this.post = post;
   }
 }
 
 export class NotAReplyError extends BaseError {
   name = "NotAReplyError";
+
+  constructor(post: Post) {
+    super(post, `Post ${post.uri} is not a reply`);
+  }
 }
 
 export class ImageGenerationError extends BaseError {
   name = "ImageGenerationError";
+
+  constructor(post: Post) {
+    super(post, `Could not generate an image for ${post.uri}`);
+  }
 }
